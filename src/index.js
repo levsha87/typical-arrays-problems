d33ef3df57e59b0b1148b606cdeca5a8cc4e0856
Roma Levsha <rm.levsha@gmail.com>
2020-03-22 03:34:42 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (typeof array != "undefined"  
+                        && array != null  
+                        && array.length != null  
+                        && array.length > 0){
+            let min = Math.min.apply(null, array);
+                       return min; }
+             else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (typeof array != "undefined"  
+                        && array != null  
+                        && array.length != null  
+                        && array.length > 0){
+
+    let max = Math.max.apply(null, array);
+        return max;
+    } else {
+    return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array != "undefined"  
+                        && array != null  
+                        && array.length != null  
+                        && array.length > 0){
+
+    let sumArr = array.reduce((sum, current) => sum + current, 0);
+    let n = array.length;
+    let result = sumArr/n;
+    return result;
+    } else {
+    return 0;
+    }
 }
